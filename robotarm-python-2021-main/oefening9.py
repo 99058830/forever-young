954ffd6b069af37573bc899d22ce93edceb71560
99058830 <99058830@mydavinci.nl>
2021-11-04 14:07:45 +0100

    fixed typos and added mastered

--- robotarm-python-2021-main/oefening9.py
+++ robotarm-python-2021-main/oefening9.py
@@ -1,12 +1,12 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 9')
-robotArm.speed = 2
-
-for i in range(4):
-    robotArm.grab()
-    for y in range(5):
-        robotArm.moveRight()
-    robotArm.drop()
-    for x in range(4):
-        robotArm.moveLeft()
+for y in range(1,5):
+    for x in range(y):
+        robotArm.grab()
+        for i in range(5):
+            robotArm.moveRight()
+        robotArm.drop()
+        for i2 in range(5):
+            robotArm.moveLeft()
+    robotArm.moveRight()
 robotArm.wait()
